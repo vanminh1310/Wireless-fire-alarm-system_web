2820c28ef49a5329102e8efaffae4bdc98316386
vanminh1310 <taminh39@gmail.com>
2020-11-29 11:28:47 +0700

    add

--- js/key.js
+++ js/key.js
@@ -40,3 +40,33 @@ firebase.auth().onAuthStateChanged(function(user) {
 
   }
 });
+setInterval(displayclock,500);
+  function displayclock(){
+    var time = new Date();
+    var hrs = time.getHours();
+    var min = time.getMinutes();
+    var sec = time.getSeconds();
+
+    if (hrs>12){
+      hrs = hrs -12;
+
+    }
+    if(hrs == 12){
+      hrs = 12;
+    }
+    if(hrs<10){
+      hrs ='0'+hrs;
+    }
+    if(min<10){
+      min = '0' +min;
+
+    }
+    if(sec<10){
+      sec= '0'+sec;
+    }
+    document.getElementById('clock').innerHTML=hrs + ':' + min +':' + sec;
+  }
+
+// var link = 'https://its-k59.firebaseapp.com/home1.html';
+//   link.split('.html')[0];
+//   window.history.replaceState( null, null, link ); 
